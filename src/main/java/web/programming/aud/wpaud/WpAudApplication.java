98cfb2e670381e35ce3581e8b88795d6bae74b13
Olgica <mirchevao@yahoo.com>
2020-10-28 17:24:04 +0100

    Initial commit

---
+++ src/main/java/web/programming/aud/wpaud/WpAudApplication.java
@@ -0,0 +1,13 @@
+package web.programming.aud.wpaud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WpAudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WpAudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/web/programming/aud/wpaud/WpAudApplicationTests.java
@@ -0,0 +1,13 @@
+package web.programming.aud.wpaud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WpAudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
